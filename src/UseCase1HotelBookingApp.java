6f87b3862ca051d626241ab06415db7edd28a3da
Manan Sharma <ms5456@srmist.edu.in>
2026-03-25 15:44:37 +0530

    Manan Added UseCase1 Application Entry

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,21 @@
+/**
+ * Hotel Booking Application Entry Point
+ * This class demonstrates the basic structure of a Java application
+ * with a welcome message.
+ *
+ * @author Manan Sharma
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("     Hotel Booking System v1.0      ");
+        System.out.println("====================================");
+
+        System.out.println("Application Started Successfully!");
+    }
+}
